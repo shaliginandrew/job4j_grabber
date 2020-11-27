9103793d4a47032da43cf344d3889b5081b9e285
shaliginandrew <shaligin.andrew@gmail.com>
2020-11-27 22:38:19 +0300

    Job c параметрами [#294138]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -6,9 +6,7 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -19,11 +17,13 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     private Properties props;
-    Connection cn;
+    private Connection cn;
+
 
     public AlertRabbit(Properties props, Connection cn) {
         this.props = props;
         this.cn = cn;
+
     }
 
     public Properties initProps() {
@@ -39,14 +39,14 @@ public class AlertRabbit {
         return props;
     }
 
-    public void timer() {
+    public void timer(Connection cn) {
 
         try {
-            List<Long> store = new ArrayList<>();
+
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDataMap data = new JobDataMap();
-            data.put("store", store);
+            data.put("connection", cn);
             JobDetail job = newJob(Rabbit.class)
                     .usingJobData(data)
                     .build();
@@ -60,7 +60,7 @@ public class AlertRabbit {
             scheduler.scheduleJob(job, trigger);
             Thread.sleep(6000);
             scheduler.shutdown();
-            System.out.println(store);
+
         } catch (Exception se) {
             se.printStackTrace();
         }
@@ -69,7 +69,6 @@ public class AlertRabbit {
 
 
     public Connection initConnect(Properties props) throws ClassNotFoundException, SQLException {
-
         try {
             Class.forName(props.getProperty("driver-class-name"));
             cn = DriverManager.getConnection(
@@ -85,10 +84,11 @@ public class AlertRabbit {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         AlertRabbit rabbit = new AlertRabbit(new Properties(), null);
         Properties properties = rabbit.initProps();
         try (Connection connection = rabbit.initConnect(properties)) {
-            rabbit.timer();
+            rabbit.timer(connection);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -104,8 +104,19 @@ public class AlertRabbit {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
-            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
-            store.add(System.currentTimeMillis());
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+
+
+            try (PreparedStatement ps = connection.prepareStatement("insert into rabbit (created) values (?)",
+                    Statement.RETURN_GENERATED_KEYS)) {
+                ps.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+                ps.execute();
+                ResultSet keys = ps.getGeneratedKeys();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
