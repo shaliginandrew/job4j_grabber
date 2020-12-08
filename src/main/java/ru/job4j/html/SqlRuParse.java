313374b1aa12097c8d25b62e55a89d201ad7365d
shaliginandrew <shaligin.andrew@gmail.com>
2020-12-08 21:48:33 +0300

    SqlRuParse [#294143]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -10,9 +10,10 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
 
     private ArrayList<Post> postData;
     private LocalDate dateStandart;
@@ -22,9 +23,6 @@ public class SqlRuParse {
         this.postData = new ArrayList<>();
     }
 
-    public void loadPostDetails(Post postDetails) {
-        postData.add(postDetails);
-    }
 
     public void dateAndTimeStandart(ParsingDate.Datap p) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-d", Locale.ENGLISH);
@@ -33,32 +31,6 @@ public class SqlRuParse {
         timeStandart = LocalTime.parse(p.resultTime, dt);
     }
 
-    public void parsingPage() throws IOException {
-        ParsingDate parsingDate = new ParsingDate();
-        String link = "https://www.sql.ru/forum/job-offers/";
-        for (int i = 1; i <= 5; i++) {
-
-
-            Document doc = Jsoup.connect(link + String.valueOf(i)).get();
-
-
-
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Element href = td.child(0);
-                if (!href.text().contains("Сообщения от модераторов")
-                        && !href.text().contains("Правила форума")
-                        && !href.text().contains("Шпаргалки")) {
-                    Element date = td.parent().child(5);
-                    ParsingDate.Datap p = parsingDate.parsing(date);
-                    dateAndTimeStandart(p);
-                    String description = description(href.attr("href"));
-                    Post postDetails = new Post(href.attr("href"), href.text(), description,  dateStandart, timeStandart);
-                    loadPostDetails(postDetails);
-                }
-            }
-        }
-    }
 
     private static String description(String url) throws IOException {
 
@@ -70,13 +42,46 @@ public class SqlRuParse {
     }
 
     public static void main(String[] args) throws Exception {
-        SqlRuParse sqlRuParse = new SqlRuParse();
-        sqlRuParse.parsingPage();
-        for (Post t : sqlRuParse.postData) {
+        SqlRuParse sql = new SqlRuParse();
+        String url = "https://www.sql.ru/forum/job-offers/";
+        for (int i = 1; i <= 5; i++) {
+            sql.list(url + i);
+        }
+        for (Post t : sql.postData) {
             System.out.println(t.getLink());
             System.out.println(t.getText());
             System.out.println(t.getCreatedDate() + " " + t.getCreatedTime());
             System.out.println(t.getDescription());
         }
+
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        postData.add(detail(link));
+        return postData;
+    }
+
+
+    @Override
+    public Post detail(String link) throws IOException {
+        Post postDetails = null;
+        ParsingDate parsingDate = new ParsingDate();
+        Document doc = Jsoup.connect(link).get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            if (!href.text().contains("Сообщения от модераторов")
+                    && !href.text().contains("Правила форума")
+                    && !href.text().contains("Шпаргалки")) {
+                Element date = td.parent().child(5);
+                ParsingDate.Datap p = parsingDate.parsing(date);
+                dateAndTimeStandart(p);
+                String description = description(href.attr("href"));
+                postDetails = new Post(href.attr("href"), href.text(), description, dateStandart, timeStandart);
+
+            }
+        }
+        return postDetails;
     }
 }
