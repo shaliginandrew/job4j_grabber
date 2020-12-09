7306b382f48906e5da707f30ef33c06e62caea27
shaliginandrew <shaligin.andrew@gmail.com>
2020-12-09 13:44:20 +0300

    Схема таблицы Post [#294147]

--- db/grabber.sql
+++ db/grabber.sql
@@ -3,9 +3,8 @@ create database grabber;
 create table post
 (
     id serial primary key,
-    link text unique,
     name text,
-    description text,
-    create_date date,
-    create_time time
+    text text,
+    link text unique,
+    created date
 );
