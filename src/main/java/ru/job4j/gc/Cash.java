96ca5b1e141bdcafcee25024f10dc8ee5059ba85
shaliginandrew <shaligin.andrew@gmail.com>
2021-04-02 19:10:31 +0300

    cash

---
+++ src/main/java/ru/job4j/gc/Cash.java
@@ -0,0 +1,13 @@
+package ru.job4j.gc;
+
+import java.util.HashMap;
+
+
+public class Cash {
+
+    HashMap<String, String> cash = new HashMap<>();
+
+    public static void main(String[] args) {
+        String path = "./job4j_grabber/src/main/resources";
+    }
+}
