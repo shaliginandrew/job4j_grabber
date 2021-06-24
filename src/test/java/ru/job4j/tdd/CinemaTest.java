ab59c25daf53b967999bf67f979de46442d1f37f
shaliginandrew <shaligin.andrew@gmail.com>
2021-06-24 18:00:00 +0300

    Что такое TDD? [#4918 #72366]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -3,8 +3,10 @@ package ru.job4j.tdd;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
@@ -37,4 +39,35 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    @Ignore
+    public void whenInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        LocalDateTime nextYear = LocalDateTime.now().plusYears(1);
+        date.set(nextYear.getYear(), nextYear.getMonthValue(), nextYear.getDayOfMonth(), nextYear.getHour(), nextYear.getMinute());
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Ignore
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1000, 1000, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Ignore
+    public void whenNotEnoughMoney() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
 }
